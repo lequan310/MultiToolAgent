097f55f9830f4c07dc4c8846861fb02dbba16fd1
Quan Le <leminhquan310@gmail.com>
2024-08-04 00:43:35 +0700

    replace hf chat model with gemini

--- backend/app/agent/agent.py
+++ backend/app/agent/agent.py
@@ -3,30 +3,19 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_community.tools.pubmed.tool import PubmedQueryRun
 from langchain_community.tools import WikipediaQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
-from langchain.agents import AgentExecutor, create_react_agent
-from app.agent.prompts.agent import agent_prompt
-from langchain.agents.format_scratchpad import format_log_to_str
+from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.memory import MemorySaver
 from app.agent.tools.pubmed import pubmed_tool
 from app.agent.tools.wikipedia import wikipedia_tool
 from app.agent.tools.tavily import search_tool
-from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, AIMessage
 from app.config import os
 
 # from app.agent.tools.get_intent import get_intent
 
-llm = HuggingFaceEndpoint(
-    repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1",
-    huggingfacehub_api_token=os.getenv("HF_API_TOKEN"),
-    max_new_tokens=1024,
-    repetition_penalty=1.03,
-    seed=42,
-    temperature=0.1,
-    cache=False,
-)
-
-chat_model = ChatHuggingFace(llm=llm)
-chat_history = []
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0)
+memory = MemorySaver()
 
 custom_tools = [pubmed_tool, wikipedia_tool, search_tool]
 tools = [
@@ -35,24 +24,14 @@ tools = [
     TavilySearchResults(max_results=3),
 ]
 
-agent = create_react_agent(llm=chat_model, tools=custom_tools, prompt=agent_prompt)
-agent_executor = AgentExecutor(
-    agent=agent,
-    tools=custom_tools,
-    verbose=True,
-    handle_parsing_errors=True,
-    # return_intermediate_steps=False,
-)
+workflow = create_react_agent(llm, tools=tools, checkpointer=memory)
 
 
-async def get_agent_response(message: str):
-    response = await agent_executor.ainvoke(
-        {"input": message, "chat_history": chat_history}
-    )
-    cleaned_response = response["output"].strip()
+def get_agent_response(message: str):
+    config = {"configurable": {"thread_id": "1"}}
+    input = {"messages": message}
 
-    chat_history.append(HumanMessage(content=message))
-    chat_history.append(AIMessage(content=cleaned_response))
-
-    print(cleaned_response)
-    return cleaned_response
+    response = workflow.invoke(input=input, config=config, stream_mode="values")
+    response = response["messages"][-1].content
+    print(response)
+    return response
--- backend/app/api/app.py
+++ backend/app/api/app.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from app.api.routes.chat import router as chat_router
+from app.api.routes.files import router as file_router
 
 app = FastAPI()
 
 app.include_router(chat_router, prefix="/chat")
+app.include_router(file_router, prefix="/files")
 
 
 @app.get("/")
